26f5462f6d18be12d3a2a03edb1cc1dc35adfd7b
sweeneysiba <sweetsiba8@gmail.com>
2018-11-20 18:38:53 +0530

    initial template layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { SidenavComponent } from './components/sidenav/sidenav.component';
+// import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [],
