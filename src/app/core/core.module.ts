a6be51680da294024623caca260693921d75ca4d
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2018-11-20 15:01:10 +0530

    Base structure : Multi lingual changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TranslateService } from './services';
+import { TranslatePipe } from './pipes';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TranslatePipe
+  ],
   imports: [
     CommonModule,
+    HttpClientModule
+  ],
+  providers: [],
+  exports: [
+    TranslatePipe
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [TranslateService]
+    };
+  }
+}
