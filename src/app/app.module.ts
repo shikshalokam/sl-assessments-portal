49a80ef6376b9c262acd6d63da0c7136a9452db6
sweeneysiba <sweetsiba8@gmail.com>
2018-11-22 19:00:23 +0530

    review fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon'; 
-import {MatMenuModule} from '@angular/material/menu';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
-import { ModulesModule } from './modules/modules.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { keycloakInitializer } from './core/auth/keycloak-initializer';
-
-import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { SidenavComponent } from './core/components/sidenav/sidenav.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-
 import { TranslateService } from './core/services/translate-service/translate.service';
+import { ModulesModule } from './modules/modules.module';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
 
 export function setupTranslateFactory(
   service: TranslateService): Function {
-  return () => service.use('ma');
+  return () => service.use('en');
 }
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SidenavComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -39,15 +30,7 @@ export function setupTranslateFactory(
     SharedModule,
     CoreModule,
     ModulesModule, 
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatCardModule,
-    HttpClientModule ,
     MatDividerModule,
-    MatSidenavModule,
-    BrowserAnimationsModule
     CoreModule.forRoot(),
     HttpClientModule
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,18 +5,39 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { TranslateService } from './services';
 import { TranslatePipe } from './pipes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
 
 @NgModule({
   declarations: [
-    TranslatePipe
+    TranslatePipe,
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule,
+    MatDividerModule,
   ],
   providers: [],
   exports: [
-    TranslatePipe
+    TranslatePipe,
+    NavbarComponent,
+    SidenavComponent
+
   ]
 })
 export class CoreModule {
