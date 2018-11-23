7be2315dffd5062ca26907c04eec9901fa9758ef
sweeneysiba <sweetsiba8@gmail.com>
2018-11-23 19:25:24 +0530

    school list table layout

--- src/app/modules/parent-interview/parent-interview-routing.module.ts
+++ src/app/modules/parent-interview/parent-interview-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ParentInterviewComponent } from './parent-interview.component';
+import { SchoolListComponent } from './school-list/school-list.component';
 
 const routes: Routes = [
   {
-    path: '', component: ParentInterviewComponent,
-    data: {}
+    path: 'parent-list', component: ParentInterviewComponent,
+    data: {},
+    children: [
+      {
+        path: 'school-list',
+        component: SchoolListComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'school-list'
+      }
+    ]
   },
-  
+  {
+    path: '**',
+    redirectTo: 'parent-list'
+  }
+
+
 ];
 
 @NgModule({
