038d66dcdaa76137e104e5407f6c8d562b00883a
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2018-11-23 13:15:39 +0530

    Build issue fix , pushing the build for github deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
+
+import { KeycloakConfig } from 'keycloak-angular';
+
+
+const base_url = 'https://dev.shikshalokam.org';
+let keycloakConfig: KeycloakConfig = {
+  url: base_url + '/auth',
+  realm: 'sunbird',
+  clientId: 'sl-ionic-connect',
+  "credentials": {
+    "secret": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkYTJiMTA5MWVlMDE0MDQ3OTdhYjRjZDI3ODJmYTFkZCJ9.olC-mJ9JVqeeIf-eyBVYciPIIsqDm46XHbKuO1GgNG0"
+  }  
+};
+
 export const environment = {
-  production: true
+  production: true,
+  keycloak: keycloakConfig,
+  base_url: base_url
 };
