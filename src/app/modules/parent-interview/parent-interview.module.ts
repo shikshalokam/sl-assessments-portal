4aa36c3016c9cd3820cb84e36faaec1b6401acb6
sweeneysiba <sweetsiba8@gmail.com>
2018-12-05 14:44:28 +0530

    parent survey template

--- src/app/modules/parent-interview/parent-interview.module.ts
+++ src/app/modules/parent-interview/parent-interview.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Injectable } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentInterviewComponent } from './parent-interview.component';
 import { ParentInterviewRoutingModule } from './parent-interview-routing.module';
@@ -10,21 +10,23 @@ import { HttpClientModule} from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { ApiService } from 'src/app/core/services/api-service';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
-// import { RouterModule } from '@angular/router';
-
+import {MatCardModule} from '@angular/material/card';
 import { ParentListComponent } from './parent-list/parent-list.component';
 import { ParentEditComponent } from './parent-edit/parent-edit.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 import { CreateFormGroup } from 'src/app/core/services/create-formgroup-service';
 import { ParentInformationComponent } from './parent-information/parent-information.component';
 @NgModule({
   declarations: [ParentInterviewComponent, SchoolListComponent, ParentListComponent, ParentEditComponent, ParentInformationComponent],
   imports: [
     CommonModule,
+    MatCardModule,
     ParentInterviewRoutingModule,
     CoreModule,
     SharedModule,
+    MatSelectModule,
     MatTableModule,
     MatButtonModule,
     MatInputModule,
@@ -37,6 +39,7 @@ import { ParentInformationComponent } from './parent-information/parent-informat
   providers:[
     ApiService,
     CreateFormGroup,
+    
     
   ]
 })
