35da2ab9effa2ae73e37d8c298d48ff1de5de8a9
sweeneysiba <sweetsiba8@gmail.com>
2018-12-05 14:44:28 +0530

    parent survey template

--- src/app/modules/parent-interview/parent-information/parent-information.component.ts
+++ src/app/modules/parent-interview/parent-information/parent-information.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api-service';
 
 @Component({
   selector: 'app-parent-information',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-information.component.scss']
 })
 export class ParentInformationComponent implements OnInit {
-
-  constructor() { }
+  parentId;
+  data;
+  selected;
+  @Output()callResponse = new EventEmitter<string>();
+  constructor( private route :ActivatedRoute,private apiFetch :ApiService ) { 
+    this.route.params.subscribe(params => {
+      this.parentId = params["id"];
+  });
+  this.showConfig();
+  }
 
   ngOnInit() {
   }
 
+  showConfig() {
+    this.apiFetch.getParentInfo(this.parentId)
+      .subscribe(data => {
+        console.log(data.result);
+        this.data = data.result
+        for(let i=0;i<data.result.length;i++){
+          if( data.result[i]['field'] == "callResponse" )
+          {
+            this.selected = data.result[i]['value'] ;
+          }
+        }
+        });
+        console.log(this.selected);
+  }
+  
+  sendcallResponse(){
+    console.log(this.selected);
+     this.callResponse.emit(this.selected);
+  }
 }
