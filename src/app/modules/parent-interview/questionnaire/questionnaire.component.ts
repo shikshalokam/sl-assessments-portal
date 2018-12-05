5b3b061b3e20d719fa4610cf8dd763aaffcf6ef7
sweeneysiba <sweetsiba8@gmail.com>
2018-12-05 21:27:38 +0530

    started with questions

--- src/app/modules/parent-interview/questionnaire/questionnaire.component.ts
+++ src/app/modules/parent-interview/questionnaire/questionnaire.component.ts
@@ -12,7 +12,10 @@ export class QuestionnaireComponent implements OnInit {
   parentInfo: any;
   parentId: string;
   schoolId: string;
-
+  x = 0; 
+  length : number;
+  selectResponse :string;
+  generalQuestion : [];
   constructor(private apiService: ApiService, private route: ActivatedRoute) { 
     this.schoolId = this.route.snapshot.paramMap.get('schoolId');
     console.log(this.schoolId)
@@ -28,14 +31,31 @@ export class QuestionnaireComponent implements OnInit {
 
   getSurveyQuestions() : void {
     this.apiService.getAssessmentQuestions(this.schoolId).subscribe(successData => {
+      console.log(successData);
       if(successData['result'].assessments){
-
+        this.generalQuestion = successData['result'].assessments[0]['generalQuestions'][0]['instanceQuestions'];
+        this.length = this.generalQuestion.length
+        console.log(this.generalQuestion);
       }
     }, errorData => {
 
     })
   }
 
+  setcallResponse(select :string){
+    if(select){
+      this.selectResponse = select;
+      console.log(this.selectResponse);
+    }
+  }
 
+  nextQuestion(){
+    if( this.x < this.length -1)
+      this.x = this.x + 1; 
+  }
+  previousQuestion(){
+    if(this.x > 0)
+      this.x = this.x - 1; 
+  }
 
 }
