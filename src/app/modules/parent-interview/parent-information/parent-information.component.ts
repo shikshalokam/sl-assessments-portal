96a368aea663144cf0b3d6fb6ec1d9908b8c30fc
sweeneysiba <sweetsiba8@gmail.com>
2018-12-05 21:27:38 +0530

    started with questions

--- src/app/modules/parent-interview/parent-information/parent-information.component.ts
+++ src/app/modules/parent-interview/parent-information/parent-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/core/services/api-service';
 
@@ -8,24 +8,24 @@ import { ApiService } from 'src/app/core/services/api-service';
   styleUrls: ['./parent-information.component.scss']
 })
 export class ParentInformationComponent implements OnInit {
-  parentId;
+  @Input()parentId;
   data;
   selected;
   @Output()callResponse = new EventEmitter<string>();
-  constructor( private route :ActivatedRoute,private apiFetch :ApiService ) { 
-    this.route.params.subscribe(params => {
-      this.parentId = params["id"];
-  });
-  this.showConfig();
+  constructor( private apiFetch :ApiService ) { 
   }
 
   ngOnInit() {
+  this.showConfig();
+
   }
 
   showConfig() {
     this.apiFetch.getParentInfo(this.parentId)
       .subscribe(data => {
         console.log(data.result);
+        console.log("result");
+
         this.data = data.result
         for(let i=0;i<data.result.length;i++){
           if( data.result[i]['field'] == "callResponse" )
