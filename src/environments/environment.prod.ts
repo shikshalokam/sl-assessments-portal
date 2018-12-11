fe48a6fb9ffa4f4201d04fb408003e2740f08e3c
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2018-12-11 13:46:06 +0530

    environment files chnages

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+
+import { KeycloakConfig } from 'keycloak-angular';
+
+
+const base_url = 'https://dev.shikshalokam.org';
+const apibaseurl = ' https://dev.shikshalokam.org/assessment/api/v1/'
+let keycloakConfig: KeycloakConfig = {
+  url: base_url + '/auth',
+  realm: '[YOUR_REALM]',
+  clientId: '[YOUR_CLIENT_ID]',
+  "credentials": {
+     "secret": "[YOUR_KEY]"
+   } 
+};
+
+export const environment = {
+  production: true,
+  keycloak: keycloakConfig,
+  base_url: base_url,
+  apibaseurl :apibaseurl
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,31 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+import { KeycloakConfig } from 'keycloak-angular';
+
+
+const base_url = 'https://dev.shikshalokam.org';
+let keycloakConfig: KeycloakConfig = {
+  url: base_url + '/auth',
+  realm: '["YOUR_REALM"]',
+  clientId: '[YOUR_CLIENTID]',
+  "credentials": {
+     "secret": "[YOUR_SECRET_KEY]"
+   }
+};
+
+export const environment = {
+  production: false,
+  keycloak: keycloakConfig,
+  base_url: base_url
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
