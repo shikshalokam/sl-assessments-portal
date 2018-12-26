56e7898f6e38c0183baffac69fb644119793f736
sweeneysiba <sweetsiba8@gmail.com>
2018-12-26 13:26:27 +0530

    resolved class name

--- src/app/core/services/parent-service/parent.service.ts
+++ src/app/core/services/parent-service/parent.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ParentConfig } from '../../../modules/parent-interview/parent-config';
 import { environment } from 'src/environments/environment';
-import { throwError } from 'rxjs';
 import { ReportConfig } from 'src/app/modules/report/report-config';
 
 export interface Config {
@@ -57,15 +56,5 @@ export class ParentService {
         return this.http.get(environment.apibaseurl + ReportConfig.GetSubmission+submissionId )
     }
 
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-            console.error('An error occurred:', error.error.message);
-        } else {
-            console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
-        }
-        return throwError(
-            'Something bad happened; please try again later.');
-    };
+   
 }
