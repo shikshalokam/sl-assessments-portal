509befeb4d0e93898f438fb7d84555380f05cdff
sweeneysiba <sweetsiba8@gmail.com>
2019-01-04 09:52:14 +0530

    internal access token added to download api request only

--- src/app/core/services/interceptor-service/interceptor.service.ts
+++ src/app/core/services/interceptor-service/interceptor.service.ts
@@ -14,16 +14,13 @@ export class ApiInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const downloadReportUrl = 'https://dev.shikshalokam.org/assessment/api/v1/reports/programsSubmissionStatus/DCPCR?evidenceId='
     const authToken = this.auth.getToken();
-    const authReq = req
-    .clone({ setHeaders: { "X-authenticated-user-token": authToken } })
-    .clone({setHeaders:{"internal-access-token" :  environment.downloadReportHeaderValue}})
-    // .clone({setHeaders:{"responseType":'text'}})
-    ;
-
-    // debugger;
-    console.log(authReq)
+    if (req.url.includes(downloadReportUrl)) {
+      const authReq = req.clone({ setHeaders: { "internal-access-token": environment.downloadReportHeaderValue } });
+      return next.handle(authReq);
+    }
+    const authReq = req.clone({ setHeaders: { "X-authenticated-user-token": authToken } })
     return next.handle(authReq);
-
   }
 }
