f4650f36f3e66b86ce8fdcf95115dae3b72d54ba
sweeneysiba <sweetsiba8@gmail.com>
2019-01-16 12:09:59 +0530

    temporary push

--- src/app/modules/parent-interview/school-list/school-list.component.ts
+++ src/app/modules/parent-interview/school-list/school-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { ParentConfig } from '../parent-config';
 import { ParentService } from '../../../core/services/parent-service/parent.service';
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, PageEvent } from '@angular/material';
 import { UtilityService } from 'src/app/core/services/utility-service/utility.service';
 import { environment } from 'src/environments/environment';
 
@@ -20,26 +20,29 @@ export class SchoolListComponent implements OnInit {
   schoolList;
   error: any;
   flag ;
+  hidePageSize: boolean =true;
+  pageIndex: number = 0;
+  pageSize: number = 1;
+  pageEvent: PageEvent;
+  length : number = 100;
+  pageSizeOptions = [1,50,100];
   headings = 'headings.schoolListHeading';
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private parentService: ParentService, private utility: UtilityService) {
-    this.showConfig();
+    this.callSchoolList();
   }
-  showConfig() {
+  callSchoolList() {
     this.parentService.getSchoolList()
       .subscribe(data => {
         this.schoolList = data.result;
         this.result = data.result.length;
         this.dataSource = new MatTableDataSource(data.result);
         console.log(data.result);
-        setTimeout(() =>{ this.dataSource.paginator = this.paginator,
-        console.log( this.dataSource.paginator),
-        console.log( this.paginator);
-        },0
-        );
-        // this.dataSource.paginator = this.paginator;
-
+        // setTimeout(() =>{ 
+        //   this.dataSource.paginator = this.paginator;
+        // }
+        // );
         this.utility.loaderHide()
       },
         (error) => {
@@ -52,15 +55,61 @@ export class SchoolListComponent implements OnInit {
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
   ngOnInit() {
     this.utility.loaderShow();
-    console.log(this.paginator);
+    
   }
-
+ 
+  seePaginatio(event){
+   
+  }
 
   objectKeys(obj) {
     return Object.keys(obj);
   }
 
+  onPaginateChange(event)
+  {
+    console.log(event)
+    if((event.previousPageIndex > event.pageIndex) && !(event.pageIndex != 0))
+    {
+      if( (event.previousPageIndex - event.pageIndex) > 1 )
+      {
+        this.pageIndex = 0;
+        console.log("firstpage");
+        return;
+        
+      }
+      this.pageIndex -=1;
+
+    console.log(this.pageIndex ,"changed Manually")
+
+    }
+    if ( (event.previousPageIndex < event.pageIndex)  && (event.pageIndex != (event.length/event.pageSize)) )
+    {
+
+      if( (event.pageIndex - event.previousPageIndex) > 1 )
+      {
+        this.pageIndex = event.pageIndex;
+        console.log("lastPage");
+        return;
+      }
+      this.pageIndex +=1;
+
+    console.log(this.pageIndex, "changed Manually")
+
+    }
+
+    if (event.pageSize != this.pageSize)
+    {
+      this.pageSize = event.pageSize;
+    console.log(this.pageSize , "changed Manually")
+
+    }
+    // console.log(event)
+    // this.pageIndex = 0;
+    // this.pageSize = 0;
+  }
 
 }
