345eb627b449e118aca3fc5a0c87bc765d63db0c
sweeneysiba <sweetsiba8@gmail.com>
2019-01-24 12:51:15 +0530

    added parent response type

--- src/app/modules/parent-interview/parent-list/parent-list.component.ts
+++ src/app/modules/parent-interview/parent-list/parent-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ParentService } from 'src/app/core/services/parent-service/parent.service';
-import {MatTableDataSource, MatPaginator, MatSnackBar} from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSnackBar } from '@angular/material';
 import { UtilityService } from 'src/app/core/services/utility-service/utility.service';
 @Component({
   selector: 'app-parent-list',
   templateUrl: './parent-list.component.html',
   styleUrls: ['./parent-list.component.scss']
 })
-export class ParentListComponent implements OnInit{
+export class ParentListComponent implements OnInit {
   dataSource;
   errorMessage;
   breadcrumbRoute;
@@ -16,91 +16,95 @@ export class ParentListComponent implements OnInit{
   parentList;
   result;
   headings = "headings.parentListHeading";
-  schoolName ;
+  schoolName;
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  smallScreen =false;
   
 
-  constructor( private route :ActivatedRoute,private snackBar : MatSnackBar,private parentService :ParentService,private utility : UtilityService ) { 
+  constructor(private route: ActivatedRoute, private snackBar: MatSnackBar, private parentService: ParentService, private utility: UtilityService) {
     this.route.params.subscribe(params => {
       this.schoolId = params["id"];
-      this.schoolName =params["name"];
+      this.schoolName = params["name"];
 
-  });
+    });
   }
-  schoolId :any;
-  submissionId:any;
-  isProdEnvironment:string;
-  atleastOneComplete : boolean;
+  schoolId: any;
+  submissionId: any;
+  isProdEnvironment: string;
+  atleastOneComplete: boolean;
   // displayedColumns: string[] = ['studentName','name', 'phone1','gender','address','grade','typeLabel' ,'programId','type'];
-  displayedColumns: string[] = ['studentName','name', 'phone1','gender','typeLabel' ,'programId','type'];
+  displayedColumns: string[] = ['studentName', 'name', 'phone1', 'gender', 'typeLabel', 'callResponse','programId', 'type'];
 
- 
+
   ngOnInit() {
+    if (window.screen.width < 760) { // 768px portrait
+      this.smallScreen = true;
+      console.log(this.smallScreen)
+    }
     this.utility.loaderShow();
-    this.breadcrumbRoute = [ 
+    this.breadcrumbRoute = [
       {
-        label : "Parent List",
-        url : "/parent/parent-list/",
-        id : this.schoolId
+        label: "Parent List",
+        url: "/parent/parent-list/",
+        id: this.schoolId
       },
-  ];
-  this.showConfig();
- }
- applyFilter(filterValue: string) {
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
- showConfig() {
-  this.parentService.getParentList(this.schoolId )
+    ];
+    this.showConfig();
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  showConfig() {
+    this.parentService.getParentList(this.schoolId)
       .subscribe(data => {
         this.parentList = data['result'];
-        data['result'].forEach(element=>{
-          if(element['callResponse'] == 'R7')
-          {
+        data['result'].forEach(element => {
+          if (element['callResponse'] == 'R7') {
             this.atleastOneComplete = true;
           }
         })
-        this.result=data['result'].length;
+        this.result = data['result'].length;
         this.dataSource = new MatTableDataSource(data['result']);
         setTimeout(() => this.dataSource.paginator = this.paginator);
         this.utility.loaderHide();
       },
-      (error)=>{
+        (error) => {
+          this.error = error;
+          this.utility.loaderHide();
+        });
+  }
+  parentInterviewSubmit(flag: boolean) {
+    if (flag) {
+      this.utility.loaderShow();
+      this.parentService.getAssessmentQuestions(this.schoolId).subscribe(successData => {
+        this.submissionId = successData['result'].assessments[0]['submissionId'];
+        console.log(this.submissionId);
+        this.parentService.parentInterviewSubmission(this.submissionId)
+          .subscribe(successData => {
+            console.log(successData);
+            this.utility.loaderHide();
+            this.snackBar.open(successData['message'], "Ok", { duration: 9000 });
+
+            this.utility.onBack();
+          },
+            (error) => {
+              this.utility.loaderHide();
+
+              this.snackBar.open(error['message'], "Ok", { duration: 9000 });
+
+            });
+      }, (error) => {
+        this.utility.loaderHide();
+        this.snackBar.open(error['message'], "Ok", { duration: 9000 });
         this.error = error;
-        this.utility.loaderHide();
-      });
-}
-parentInterviewSubmit(flag :boolean){
-  if(flag){
-    this.utility.loaderShow();
-    this.parentService.getAssessmentQuestions(this.schoolId).subscribe(successData => {
-      this.submissionId =  successData['result'].assessments[0]['submissionId'];
-      console.log(this.submissionId);
-      this.parentService.parentInterviewSubmission(this.submissionId)
-      .subscribe(successData =>{
-        console.log(successData);
-        this.utility.loaderHide();
-        this.snackBar.open(successData['message'], "Ok", {duration: 9000});
+      }
+      );
 
-        this.utility.onBack();
-      },
-      (error)=>{
-        this.utility.loaderHide();
-
-        this.snackBar.open(error['message'], "Ok", {duration: 9000});
-
-      });
-    },(error)=>{
-      this.utility.loaderHide();
-      this.snackBar.open(error['message'], "Ok", {duration: 9000});
-      this.error = error;
     }
-    );
 
   }
-
-}
-objectKeys(obj) {
-  return Object.keys(obj);
-}
+  objectKeys(obj) {
+    return Object.keys(obj);
+  }
 
 }
