671ee90fd1b856986564b0ab5a59b56d95167e01
sweeneysiba <sweetsiba8@gmail.com>
2019-02-13 16:28:50 +0530

    api pagination for view schools done with session expired loggedout

--- src/app/core/services/interceptor-service/interceptor.service.ts
+++ src/app/core/services/interceptor-service/interceptor.service.ts
@@ -44,7 +44,6 @@ export class ApiInterceptor implements HttpInterceptor {
            
           },
           () => {
-            // console.log("completed successfully"); 
           }
         ));
   }
