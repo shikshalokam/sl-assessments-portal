52e55b766b54ec1be0766ddf184cadfc65a51fc0
sweeneysiba <sweetsiba8@gmail.com>
2019-02-13 16:28:50 +0530

    api pagination for view schools done with session expired loggedout

--- src/app/core/services/interceptor-service/interceptor.service.ts
+++ src/app/core/services/interceptor-service/interceptor.service.ts
@@ -4,14 +4,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpInterceptor,
+  HttpEvent,
+  HttpErrorResponse,
 } from "@angular/common/http";
 import { AuthService } from "../auth/auth.service";
 import { environment } from "src/environments/environment";
+import { tap } from "rxjs/internal/operators/tap";
+import { MatSnackBar } from "@angular/material";
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,private snackBar :MatSnackBar) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const downloadReportUrl = 'programsSubmissionStatus/DCPCR?evidenceId='
@@ -22,7 +26,27 @@ export class ApiInterceptor implements HttpInterceptor {
         const authReq = req.clone({setHeaders:{"internal-access-token" :  environment.downloadReportHeaderValue}});
         return next.handle(authReq);
       }
-      const authReq = req.clone({ setHeaders: { "X-authenticated-user-token": authToken } })
-        return next.handle(authReq);
+      const authReq = req.clone({ setHeaders: { "X-authenticsadated-user-token": authToken } })
+        return next.handle(authReq)
+        .pipe(tap(
+          (response: HttpEvent<any>) => {
+          },
+          (error: HttpErrorResponse) => {
+            // console.log(error.status);
+            if(error.status === 401){
+             setTimeout(()=>{
+              this.snackBar.open("Your Session Expired, Login To Continue.", "Ok", { duration: 3000 });
+              setTimeout( () => {
+                this.auth.getLogout();     
+              },3000);
+             },3000
+             )
+            }
+           
+          },
+          () => {
+            // console.log("completed successfully"); 
+          }
+        ));
   }
 }
